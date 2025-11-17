b07e8e5c66bee0f0b18712c923efbf012f7c9030
Sebastian K <sebastian.king19@education.nsw.gov.au>
2025-11-17 12:06:13 +1100

    ultrasonic

--- UltrasonicGlasses/ultrasonicsensor/ultrasonicsensor.ino
+++ UltrasonicGlasses/ultrasonicsensor/ultrasonicsensor.ino
@@ -1,9 +1,18 @@
+#include "Ultrasonic.h"
+
+Ultrasonic myUltraSonicSensor(5);
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  unsigned long RangeInCentimeters;
 
+  RangeInCentimeters = myUltraSonicSensor.distanceRead(); // two measurements 
+  Serial.print(RangeInCentimeters); // 0~400cm
+  Serial.println(" cm");
+  delay(250);
 }
