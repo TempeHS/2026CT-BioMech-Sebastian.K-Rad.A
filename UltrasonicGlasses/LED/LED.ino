26f9051e6b34aba3e3644e5d356efd6b08a100f7
Sebastian K <sebastian.king19@education.nsw.gov.au>
2025-11-19 11:55:53 +1100

    stu happu

--- UltrasonicGlasses/LED/LED.ino
+++ UltrasonicGlasses/LED/LED.ino
@@ -1,20 +1,23 @@
 
 unsigned static int redLED = 4;
 unsigned static int buttonPIN = 6; 
+int onSTATE = 1;
 
 void setup() {
   // put your setup code here, to run once:
-  SeriaI.begin(9600);
+  Serial.begin(9600);
   pinMode(redLED, OUTPUT);
-
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- if onSTATE == true {
-  then redLED = HIGH
+ if (onSTATE == 2) {
+  analogWrite(redLED, 255); 
+ } else if (onSTATE == 1) {
+  analogWrite(redLED, 100); 
  } else {
-  onSTATE = true
+  analogWrite(redLED, 0); 
  }
+
+ delay(100);
 }
