02834b65ee98c91c5ad209636392b19bd5e5b9f4
Sebastian K <sebastian.king19@education.nsw.gov.au>
2025-12-01 12:15:46 +1100

    IT WORKS

--- UltrasonicGlasses/LED/LED.ino
+++ UltrasonicGlasses/LED/LED.ino
@@ -1,81 +1,73 @@
-unsigned static int greenLED = 9;
-unsigned static int redLED = 5;
-unsigned static int buttonPIN = 6; 
+unsigned static int greenLED = 5;
+unsigned static int redLED = 3;
+unsigned static int LbuttonPIN = 6;
+unsigned static int RbuttonPIN = 7;
+
+// LEFT LED IS RED
+// RIGHT LED IS GREEN
 
 int leftLED = 1;
 int rightLED = 2;
 
-//int lastButtonState = HIGH;  
-//unsigned long lastDebounceTime = 0;   
-//unsigned long debounceDelay = 50; 
+int LbuttonState = 0;
+int LlastButtonState = HIGH;
 
-// LEFT LED IS RED
-// RIGHT LED IS GREEN
+int RbuttonState = 0;
+int RlastButtonState = HIGH;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(redLED, OUTPUT);
-  pinMode(buttonPIN, INPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(LbuttonPIN, INPUT);
+  pinMode(RbuttonPIN, INPUT);
   Serial.println("baud rate is at 9600");
 }
 
 
-
-
 void loop() {
-  // put your main code here, to run repeatedly:
-//
-// //AI code
-//   int reading = digitalRead(buttonPIN);
-//
-// // Simple debounce logic
-// if (reading != lastButtonState) {
-//   lastDebounceTime = millis();
-// }
-//
-// if ((millis() - lastDebounceTime) > debounceDelay) {
-//   if (reading == LOW && lastButtonState == HIGH) {
-//     // Button was just pressed
-//     leftLED = (leftLED + 1) % 3;  // Cycle through 0, 1, 2
-//     Serial.print("leftLED: ");
-//     Serial.println(leftLED);
-//   }
-// }
-// lastButtonState = reading;
+  // LEFT
+  rightloop();
 
+  int LbuttonPressed = digitalRead(LbuttonPIN);
 
-//
-//int buttonState = digitalRead(buttonPIN);
-//if (buttonState != leftLED) {
-//  leftLED = buttonState;
-//}
-//
-//int lastButtonState = HIGH; // Tracks previous button state
-//  lastButtonState = buttonState;
-//  // Detect button press (HIGH to LOW)
-//  if (lastButtonState == HIGH && buttonState == LOW) {
-//    leftLED = (leftLED + 1) % 3; // Cycle through 0, 1, 2
-//  }
-//
+  if (LlastButtonState == HIGH && LbuttonPressed == LOW) {
+    LbuttonState = (LbuttonState + 1) % 3;
+  }
+  LlastButtonState = LbuttonPressed;
 
-//Serial.println(buttonState);
+  leftLED = LbuttonState;
 
- if (rightLED == 2) {
-  analogWrite(greenLED, 255); 
- } else if (rightLED == 1) {
-  analogWrite(greenLED, 100); 
- } else {
-  analogWrite(greenLED, 0); 
- }
+  if (leftLED == 2) {
+    analogWrite(redLED, 255);
+  } else if (leftLED == 1) {
+    analogWrite(redLED, 100);
+  } else {
+    analogWrite(redLED, 0);
+  }
+}
 
- if (leftLED == 2) {
-  analogWrite(redLED, 255); 
- } else if (leftLED == 1) {
-  analogWrite(redLED, 100); 
- } else {
-  analogWrite(redLED, 0); 
- }
+void rightloop() {
+  // RIGHT
 
- delay(300);
+  if (rightLED == 2) {
+    analogWrite(greenLED, 255);
+  } else if (rightLED == 1) {
+    analogWrite(greenLED, 100);
+  } else {
+    analogWrite(greenLED, 0);
+  }
+
+  int RbuttonPressed = digitalRead(RbuttonPIN);
+
+  if (RlastButtonState == HIGH && RbuttonPressed == LOW) {
+    RbuttonState = (RbuttonState + 1) % 3;
+  }
+  RlastButtonState = RbuttonPressed;
+
+  rightLED = RbuttonState;
+
+  delay(100);
 }
